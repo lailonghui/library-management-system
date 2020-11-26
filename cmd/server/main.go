3db9ef1b15d5e9d017b3430013a38a238b3e74fd
lailonghui <624758224@qq.com>
2020-11-26 13:59:52 +0800

    initialize the project

---
+++ cmd/server/main.go
@@ -0,0 +1,13 @@
+/*
+@Time : 2020/11/26 13:58
+@Author : lai
+@Description :
+@File : main
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(111)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module lai.com/library-management-system
+
+go 1.13
